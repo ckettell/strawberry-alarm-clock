037bd04f1a9a24dd08a94cf3602912c2846cd2dc
Charles Kettell <charleskettell@hotmail.co.uk>
2019-09-05 14:54:30 +0100

    alarm notification works

--- src/Clock.js
+++ src/Clock.js
@@ -13,6 +13,33 @@ import moment from "moment";
 
 import DateTimePicker from "react-native-modal-datetime-picker";
 
+const fireDate = ReactNativeAN.parseDate(new Date(Date.now() + 1000));     // set the fire date for 1 second from now
+
+
+const alarmNotifData = {
+	id: "12345",                                  // Required
+	title: "My Notification Title",               // Required
+	message: "My Notification Message",           // Required
+	channel: "my_channel_id",                     // Required. Same id as specified in MainApplication's onCreate method
+	ticker: "My Notification Ticker",
+	auto_cancel: true,                            // default: true
+	vibrate: true,
+	vibration: 100,                               // default: 100, no vibration if vibrate: false
+	small_icon: "ic_launcher",                    // Required
+	large_icon: "ic_launcher",
+	play_sound: true,
+	sound_name: null,                             // Plays custom notification ringtone if sound_name: null
+	color: "red",
+	schedule_once: true,                          // Works with ReactNativeAN.scheduleAlarm so alarm fires once
+	tag: 'some_tag',
+	fire_date: fireDate,                          // Date for firing alarm, Required for ReactNativeAN.scheduleAlarm.
+
+	// You can add any additional data that is important for the notification
+	// It will be added to the PendingIntent along with the rest of the bundle.
+	// e.g.
+  	data: { foo: "bar" },
+};
+
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
