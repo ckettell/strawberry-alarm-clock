c94907176e168206eb896b0c462a3cc70a2d58d6
Freddie <f.varnals@gmail.com>
2019-09-06 13:40:09 +0100

    Test for Button to set alarm.
    For this it was necessary to install Enzyme (I used the package enzyme-adapter-react-15), installed using ```npm install jest-environment-enzyme jest-enzyme enzyme-adapter-react-16 --save-dev```
    
    To resolve error (Cannot find module 'react-dom') I had to install manually using:
    ```npm install --save-dev react-dom enzyme```
    
    The package.json file also had to be updated in order to allow Jest to work correctly with Enzyme using React Native.

--- __tests__/Clock-test.js
+++ __tests__/Clock-test.js
@@ -1,16 +1,27 @@
 import 'react-native';
 import React from 'react';
 
+import {
+	View,
+	Text,
+	StyleSheet,
+	StatusBar,
+	Button,
+	Alert
+} from "react-native";
+
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 import { render } from 'react-native-testing-library';
 import renderer from 'react-test-renderer';
 import Clock from '../src/Clock'
 import moment from 'moment';
 
 it('renders correctly', () => {
-  const time = renderer.create(
+  const clock = renderer.create(
     <Clock />
   ).toJSON();
-  expect(time).toMatchSnapshot();
+  expect(clock).toMatchSnapshot();
 });
 
 it('shows current time', () => {
@@ -18,4 +29,11 @@ it('shows current time', () => {
   const { getByTestId } = render(<Clock />);
   const displayed_time = (getByTestId('clock_time').props.children);
   expect(displayed_time).toEqual(current_time);
+});
+
+describe("Alarm Set Button", () => {
+  test("renders", () => {
+    const wrapper = shallow(<Button title="When would you like to wakeup?" />);
+    expect(wrapper.exists()).toBe(true);
+  });
 });
