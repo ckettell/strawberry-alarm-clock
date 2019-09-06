209b462b17a80f702004ecab50d8d0d4d6d0f5a6
Freddie <f.varnals@gmail.com>
2019-09-06 12:57:51 +0100

    Test to confirm clock shows current time

--- __tests__/Clock-test.js
+++ __tests__/Clock-test.js
@@ -1,12 +1,21 @@
 import 'react-native';
 import React from 'react';
 
+import { render } from 'react-native-testing-library';
 import renderer from 'react-test-renderer';
 import Clock from '../src/Clock'
+import moment from 'moment';
 
 it('renders correctly', () => {
   const time = renderer.create(
     <Clock />
   ).toJSON();
   expect(time).toMatchSnapshot();
+});
+
+it('shows current time', () => {
+  const current_time = moment().format("LTS");
+  const { getByTestId } = render(<Clock />);
+  const displayed_time = (getByTestId('clock_time').props.children);
+  expect(displayed_time).toEqual(current_time);
 });
