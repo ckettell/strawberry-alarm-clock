a6310d7968e22694b29843fc97b921e995c85d0a
Charles Kettell <charleskettell@hotmail.co.uk>
2019-09-07 14:22:24 +0100

    downloaded the packages needed for navigating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import Clock from './Clock'
+import AppNavigator from '../navigation/index'
 
 
 export default class App extends Component {
---
+++ src/Time.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import { View, Text } from 'react-native'
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+
+export default class Time extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Time</Text>
+        <Button
+        title="Go to clock"
+        onPress={() => this.props.navigation.navigate('Clock')}
+      />
+      </View>
+    );
+  }
+}
