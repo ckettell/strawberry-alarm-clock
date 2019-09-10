80406bfc39c10c309886d4f565fbb14d4d3afe8b
Freddie <f.varnals@gmail.com>
2019-09-10 11:55:29 +0100

    Current weather now uses phone's location for API request.
    Co-authored-by: Jia Tian Xia <xiajtn@gmail.com>

--- src/location/weather.js
+++ src/location/weather.js
@@ -6,22 +6,20 @@ export default class Weather extends Component {
 constructor(props) {
   super(props);
   this.state = {
-    locationLat: '59.676029',
-    locationLong: '18.765110',
     apiKey: '0fd818ea25024ad2f4461e80460c8d1a',
     weather: '',
   }
 };
 
   reportWeather = () => {
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${this.state.locationLat}&lon=${this.state.locationLong}&&APPID=${this.state.apiKey}`)
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${this.props.location.latitude}&lon=${this.props.location.longitude}&&APPID=${this.state.apiKey}`)
     .then( (response) => response.json() )
     .then( (responseJson) => {
       this.setState({
         weather: responseJson.list[0].weather[0].main
 
       })
-
+      
     })
   }
 
